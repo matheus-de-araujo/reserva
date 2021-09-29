9159a4ecd155df3a096da3895ebadb53927c358f
Matheus de Araujo <matheus@pop-os.localdomain>
2021-09-29 17:55:26 -0300

    initial commit

---
+++ src/main/java/br/com/reserva/room/domain/Room.java
@@ -0,0 +1,27 @@
+package br.com.reserva.room.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Room implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nome;
+    private String cpf;
+}
